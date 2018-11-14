8a009bf285877b038fc5bdfe368fd6ead096135f
ZReKoJ <zhong.9745@gmail.com>
2018-11-14 17:22:52 +0100

    include logger for express

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 "use strict"
 
-const express = require("express");
 const fs = require("fs");
+const express = require("express");
+const expressLogging = require("express-logging");
+const logger = require("logops");
 const config = require("./public/js/config");
 const messages = require("./public/js/messages");
 const {
@@ -9,6 +11,25 @@ const {
 } = require("./public/js/utils");
 
 const app = express();
+
+logger.format = logger.formatters.dev;
+app.use(expressLogging(logger));
+
+app.get("/css/utils.css", (request, response) => {
+    fs.readFile("./public/css/utils.css", (err, content) => {
+        if (err) {
+            response.status = 404;
+            response.setHeader("Content-Type", "text/css");
+            response.end();
+        }
+        else {
+            response.status = 200;
+            response.setHeader("Content-Type", "text/css");
+            response.write(content);
+            response.end();
+        }
+    });
+});
 
 app.get("/", (request, response) => {
     fs.readFile("./public/html/login-register.html", (err, content) => {
@@ -24,11 +45,27 @@ app.get("/", (request, response) => {
             response.end();
         }
     });
-})
+});
+
+app.get("/css/login-register.css", (request, response) => {
+    fs.readFile("./public/css/login-register.css", (err, content) => {
+        if (err) {
+            response.status = 404;
+            response.setHeader("Content-Type", "text/css");
+            response.end();
+        }
+        else {
+            response.status = 200;
+            response.setHeader("Content-Type", "text/css");
+            response.write(content);
+            response.end();
+        }
+    });
+});
 
 app.listen(config.port, (err) => {
     if (err) {
-        console.log(
+        logger.error(
             Strings.transform(
                 messages[config.language].serverInitiateError, {
                     errorMessage: err.message
@@ -36,7 +73,7 @@ app.listen(config.port, (err) => {
             )
         );
     } else {
-        console.log(
+        logger.info(
             Strings.transform(
                 messages[config.language].serverListening, {
                     port: config.port
