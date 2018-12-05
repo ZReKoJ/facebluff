3380ff5f14b8d3314cc541cf0821dd63e863eab2
Dacuna <dacuna@ucm.es>
2018-12-05 10:42:42 +0100

    half-done profile information shown

--- public/js/router/profile-router.js
+++ public/js/router/profile-router.js
@@ -4,9 +4,15 @@
 const {
     MiddleWares
 } = require("../utils");
+const DAO = require("../database/dao");
+const config = require("../../../config");
+const messages = require("../messages");
+const Entity = require("../database/entity");
 
 // public libs
 const express = require("express");
+const mysql = require("mysql");
+const pool = mysql.createPool(config.mysqlConfig);
 
 const router = express.Router();
 
@@ -14,7 +20,47 @@ router.use(MiddleWares.checkUserLogged);
 
 router.get("/", (request, response) => {
     response.status(200);
-    response.render("profile");
+    new DAO.friend(pool).findFriends(request.session.currentUser.id, (err, result) => {
+        if (err) {
+            throw err;
+        } else {
+            new DAO.question(pool).findBy({
+                userid: request.session.currentUser.id
+            }, (err, result2) => {
+                if (err) {
+                    throw err;
+                } else {
+                    new DAO.user(pool).findBy({
+                        id: request.session.currentUser.id
+                    }, (err, result3) => {
+                        if (err) {
+                            throw err;
+                        } else {
+                            let user = {
+                                id: request.session.currentUser.id,
+                                username: result3[0].username,
+                                email: result3[0].email,
+                                password: result3[0].password,
+                                birthdate: result3[0].birthdate,
+                                gender:result3[0].gender,
+                                img: result3[0].img,
+                                description: result3[0].description,
+                                score: result3[0].score
+                            };
+                            response.render("profile", {
+                                friends: result.length,
+                                questions: result2.length,
+                                user: user
+                            });
+                        }
+                    })
+
+                }
+            })
+
+        }
+    })
+
 });
 
 module.exports = {
